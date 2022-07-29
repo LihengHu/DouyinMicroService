b80fd2835e1d719ee45f21d2bab2b89732ebe2b2
huliheng <821786679@qq.com>
2022-07-29 15:02:38 +0800

    点赞操作

--- douyin-favourite/src/main/java/com/douyin/service/FavouriteService.java
+++ douyin-favourite/src/main/java/com/douyin/service/FavouriteService.java
@@ -1,5 +1,7 @@
 package com.douyin.service;
 
+import com.douyin.Dao.FavouriteMapper;
+import com.douyin.entity.Video;
 import com.douyin.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -7,28 +9,46 @@ import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class FavouriteService {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private FavouriteMapper favouriteMapper;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
     //点赞
+    //查缓存是否存在
     public void like(int userId , int videoId ,int action_type){
-        redisTemplate.execute(new SessionCallback() {
-            @Override
-            public Object execute(RedisOperations operations) throws DataAccessException {
-                String userLikeKey = RedisKeyUtil.getUserLikeKey(userId);
-                String videoLikeKey= RedisKeyUtil.getVideoLikeKey(videoId);
-                operations.multi();
-                if(action_type == 2){
-                    redisTemplate.opsForSet().remove(userLikeKey,videoId);
-                    redisTemplate.opsForValue().decrement(videoLikeKey);
-                }else if(action_type == 1){
-                    redisTemplate.opsForSet().add(userLikeKey,videoId);
-                    redisTemplate.opsForValue().increment(videoLikeKey);
-                }
-                return operations.exec();
+        String videoLikeKey = RedisKeyUtil.getVideoLikeKey(videoId);
+        String userLikeKey = RedisKeyUtil.getUserLikeKey(userId);
+        if(action_type == 1){
+            favouriteMapper.insertVideoId(userId,videoId);//先更新数据库
+        }else {
+            favouriteMapper.updateStatus(userId,videoId,1);
+        }
+        //删缓存
+        if(redisTemplate.hasKey(videoLikeKey)){
+            redisTemplate.delete(videoLikeKey);
+        }
+        if(redisTemplate.hasKey(userLikeKey)){
+            redisTemplate.delete(userLikeKey);
+        }
+    }
+
+    public void list(int userId){
+        String userLikeKey = RedisKeyUtil.getUserLikeKey(userId);
+        if(redisTemplate.hasKey(userLikeKey)){
+            Set<Integer> members = redisTemplate.opsForSet().members(userLikeKey);
+        }else{
+            List<Integer> vids = favouriteMapper.selectVideoIdByUserId(userId);
+            for (Integer vid:
+                 vids) {
+                redisTemplate.opsForSet().add(userLikeKey,vid);
             }
-        });
+        }
     }
 }
